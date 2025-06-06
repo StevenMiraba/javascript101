7fdc50318c2f5dba0234abee8a7879788e279904
StevenMiraba <150202191+StevenMiraba@users.noreply.github.com>
2025-06-06 12:31:24 -0500

    Update file01.js

--- src/file01.js
+++ src/file01.js
@@ -17,8 +17,8 @@ import { setTitle } from "./functions.js";
  * Declare la variable local pageId con el ID del elemento HTML donde se mostrará el título.
  * Referencia: https://javascript.info/variables
  */
-let pageTitle = "Datos comparativos de frameworks JavaScript modernos";
-let pageId = "title";
+const pageTitle = "Datos comparativos de frameworks JavaScript modernos";
+const pageId = "title";
 
 // Llama a la función setTitle para establecer el contenido del elemento HTML con el ID especificado
 
