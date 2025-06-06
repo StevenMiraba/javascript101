30f3e88f50488778350ce0691626d32a353df5a7
StevenMiraba <150202191+StevenMiraba@users.noreply.github.com>
2025-06-06 13:16:37 -0500

    Update file01.js

--- src/file01.js
+++ src/file01.js
@@ -22,4 +22,4 @@ const pageId = "title";
 
 // Llama a la función setTitle para establecer el contenido del elemento HTML con el ID especificado
 
-setTitle(pageTitle, pageId);
+setTitle(pageId, pageTitle);
